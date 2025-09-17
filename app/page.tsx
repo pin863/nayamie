b631bbf1a3697d2080fcba98a84c8b5bca738046
pin863 <chika67zz@gmail.com>
2025-09-17 17:36:55 +0900

    トップページにfetch追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,51 @@
+"use client";
+
 // ==========================================================
 // トップページ
 // ==========================================================
 
+import { useEffect, useState } from "react";
 import Searchbar from "@/app/components/Searchbar";
 import PostComponent from "@/app/components/PostComponent";
 import { getAllPosts } from "@/app/utils/supabaseFunctions";
 import type { Post } from "@/types/type";
 
-export default async function Home() {
-  // 最近の投稿のデータ取得
-  const posts: Post[] = await getAllPosts();
+export default function Home() {
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchPosts = async () => {
+    setLoading(true);
+    const data = await getAllPosts();
+    setPosts(data);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
   return (
     <main className="py-6">
-      {/* 検索バー */}
       <Searchbar />
       <div className="space-y-4 mt-5">
         <h3>最近の投稿</h3>
-        <div className="grid grid-cols-3 gap-4 ">
-          {/* 投稿 */}
-          {posts.map((post) => (
-            <PostComponent
-              key={post.id}
-              {...post}
-              date={post.date}
-              showButton={true}
-              href={`/posts/${post.id}`}
-              truncate={true}
-            />
-          ))}
-        </div>
+        {loading ? (
+          <p>読み込み中...</p>
+        ) : (
+          <div className="grid grid-cols-3 gap-4">
+            {posts.map((post) => (
+              <PostComponent
+                key={post.id}
+                {...post}
+                date={post.date}
+                showButton={true}
+                href={`/posts/${post.id}`}
+                truncate={true}
+              />
+            ))}
+          </div>
+        )}
       </div>
     </main>
   );
